cd7327de2f3f1cdea1b0549910c562f15f12b9ef
saigal_amatya <saigal@noveltytechnology.com>
2019-10-24 13:50:39 +0545

    hot fix

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/opentok-web-chat/dist'));
 
 app.get('/*', function (req, res) {
 
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/opentok-web-chat/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
